a751391be5c31ae6a17df242c01ab57fb4310ec3
Denissant <denissanturyan@gmail.com>
2021-04-21 03:35:30 +0400

    structuring, blueprints, flask-login, admin

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import app
-from routes import list_pages, list_people, list_posts, auth, success_register, profile, logoff
-from flask import session
-
-app.config['SECRET_KEY'] = b"safiiasmfmiq2o4182u9ejdqwr89214y"
+from app.routes import list_pages, auth, success_register, logoff
+from app.admin.admin_model import AdminModelView
 
 if __name__ == '__main__':
     app.run(port=5080, debug=True)
